09a978870e7963ae1ee8a3cf86e32d9147a31d05
thiago_silva_cirne <digimagemcftv@gmail.com>
2023-05-02 23:07:03 -0300

    exercicio condicoes if/else

---
+++ fundamentos/secao3.1-introducao-javascript/condicoes-if-else.js
@@ -0,0 +1,9 @@
+let nota = 41;
+
+if(nota >= 80) {
+    console.log("Parabéns você esta aprovada");
+} else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+} else {
+   console.log("infelizmente voce reprovou");
+}
