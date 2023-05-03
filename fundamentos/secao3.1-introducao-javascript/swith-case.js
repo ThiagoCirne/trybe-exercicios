e227cbe63be517c020e53937b1adff39fe647e36
thiago_silva_cirne <digimagemcftv@gmail.com>
2023-05-03 20:49:45 -0300

    swith-case

---
+++ fundamentos/secao3.1-introducao-javascript/swith-case.js
@@ -0,0 +1,15 @@
+let resultado = 'Aprovada';
+
+switch (resultado) {
+    case 'Aprovada':
+        break;
+        console.log("Parabén, você está no grupo de pessoas aprovadas!");
+    case 'Lista':
+        break;
+        console.log("Você está na nossa lista de espera");
+    case 'reprovada':
+        break;
+        console.log("Infelizmente, você reprovou");  
+    default:
+        console.log("Informação incorreta"); 
+}
