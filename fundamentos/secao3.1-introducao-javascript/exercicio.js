7c73dc3fd552991db3d0fa0e440c6aa9b9a5592c
thiago_silva_cirne <digimagemcftv@gmail.com>
2023-05-03 21:40:09 -0300

    exercicio 1

---
+++ fundamentos/secao3.1-introducao-javascript/exercicio.js
@@ -0,0 +1,10 @@
+/*1 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. 
+Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:*/
+let a = 5;
+let b = 8;
+
+console.log("Adição :", a + b);
+console.log("Subtração :", a - b);
+console.log("Multiplicação :", a * b);
+console.log("Divisão :", a / b);
+console.log("Módulo :", b % a);
