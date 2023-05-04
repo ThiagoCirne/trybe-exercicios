4bff43b8dcfab8c2d368faa197939388fa46cf7a
thiago_silva_cirne <digimagemcftv@gmail.com>
2023-05-03 21:49:54 -0300

    questao 2

--- fundamentos/secao3.1-introducao-javascript/exercicio.js
+++ fundamentos/secao3.1-introducao-javascript/exercicio.js
@@ -1,5 +1,5 @@
 /*1 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. 
-Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:*/
+Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
 let a = 5;
 let b = 8;
 
@@ -8,3 +8,15 @@ console.log("Subtração :", a - b);
 console.log("Multiplicação :", a * b);
 console.log("Divisão :", a / b);
 console.log("Módulo :", b % a);
+*/
+/*2 - Utilize if/else para escrever um código que retorne o maior de dois números. 
+Defina, no começo do seu código, duas variáveis com os valores que serão comparados.*/
+
+/*let c = 25;
+let d = 34;
+
+if (c > d) {
+    console.log("O número :", c ,"é maior");
+} else 
+    console.log("o numero :", d ,"é maior");
+*/
